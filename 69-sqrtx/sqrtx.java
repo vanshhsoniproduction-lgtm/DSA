397ff4dc6eb9462174224d4562f314ce46abc878
Vansh Soni <vanshhsoni.production@gmail.com>
2026-07-29 11:23:05 +0530

    Time: 1 ms (97.43%) | Memory: 42.7 MB (23.59%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,15 +1,32 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x== 2147483647){
-            return 46340;
+    //     if(x== 2147483647){
+    //         return 46340;
+    //     }
+    //     for(int i=0;i<x+1;i++){
+    //         if((long)(i * i)==x){
+    //             return i;
+    //         }
+    //         if((long)(i * i)>x)
+    //         return i-1;
+    //         }
+    // return 0;
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        for(int i=0;i<x+1;i++){
-            if((long)(i * i)==x){
-                return i;
-            }
-            if((long)(i * i)>x)
-            return i-1;
-            }
-    return 0;
+
+        return high;
     }
 }
