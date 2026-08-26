be0751f51c28cfb187c70d60e4030401e89a5a88
Vansh Soni <vanshhsoni.production@gmail.com>
2026-08-26 09:28:12 +0530

    Time: 8 ms (0.29%) | Memory: 43.4 MB (12.16%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode answer = new ListNode(-1);
+        answer.next = head;
+        ListNode temp = head;
+        ListNode prev = answer;
+        
+        while(temp != null && temp.next != null){
+            ListNode swap = temp.next;
+            ListNode next = temp.next.next;
+            prev.next = swap;
+            swap.next = temp;
+            temp.next = next;
+            prev = temp;
+            temp = next;
+            
+        }
+        temp = answer.next;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+
+        return answer.next;
+    }
+}
